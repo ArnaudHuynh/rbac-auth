c9add789506615729d7ba2a47e92aeaaa19f594e
Huynh Ngoc Thien An <anhuynh@192.168.1.83>
2025-03-26 23:10:49 +0700

    initial commit

---
+++ src/main/java/com/ng/authen/rbac_app/RbacAppApplication.java
@@ -0,0 +1,13 @@
+package com.ng.authen.rbac_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ng/authen/rbac_app/RbacAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.authen.rbac_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
